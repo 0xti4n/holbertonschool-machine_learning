87a7bb14b7d50005031d5ea360f9b3fc3d5b0aa8
Cristian G <1154@holbertonschool.com>
2020-07-13 14:26:04 +0000

    Adding task 2

---
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+
+def matrix_shape(matrix):
+    """ function that calculates
+        the shape of a matrix
+    """
+    shape_matrix = []
+    len_matrix = len(matrix)
+
+    shape_matrix.append(len_matrix)
+    shape_matrix.append(len(matrix[0]))
+
+    try:
+        shape_matrix.append(len(matrix[0][0]))
+    except TypeError:
+        pass
+
+    return shape_matrix
