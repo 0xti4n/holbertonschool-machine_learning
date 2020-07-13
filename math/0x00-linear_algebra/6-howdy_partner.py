14f703204cb3eca66d2b41c393ca155fe4f889ac
Cristian G <1154@holbertonschool.com>
2020-07-13 18:04:39 +0000

    Adding task 6

---
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""concatenates two arrays"""
+
+
+def cat_arrays(arr1, arr2):
+    """Function that concatenates two arrays"""
+
+    new_list = arr1 + arr2
+
+    return new_list
