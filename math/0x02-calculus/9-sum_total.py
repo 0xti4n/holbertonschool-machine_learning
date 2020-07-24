d11817d8541d8d79e86e96151a3cc2501a03ca06
Cristian G <1154@holbertonschool.com>
2020-07-24 14:53:29 +0000

    Updating task 9

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -7,4 +7,7 @@ def summation_i_squared(n):
 
     if n == 0:
         return 0
-    return n ** 2 + summation_i_squared(n - 1)
+    res = n * n + summation_i_squared(n - 1)
+    if not res:
+        return None
+    return res
