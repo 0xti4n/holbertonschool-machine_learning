2517a7fd4eef7a7ad38e8eb225dbd0e30b7e94b9
Cristian G <1154@holbertonschool.com>
2020-07-24 15:22:28 +0000

    Updating task 9

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -5,7 +5,7 @@
 def summation_i_squared(n):
     """Calculates summation"""
 
-    if type(n) != int:
+    if type(n) != int or n < 1:
         return None
 
     res = n * (n + 1) * (2 * n + 1) // 6
