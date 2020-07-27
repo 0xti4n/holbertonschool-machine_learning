582494cdb268d420259bcd77d3a20026d6026fd4
Cristian G <1154@holbertonschool.com>
2020-07-27 22:41:36 +0000

    adding PMF and CDF functions to poisson distribution

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -2,6 +2,13 @@
 """poisson distribution"""
 
 
+def fact(n):
+    """Factorial"""
+    if n <= 1:
+        return 1
+    return n * fact(n - 1)
+
+
 class Poisson():
     """class Poisson that represents
     a poisson distribution
@@ -20,3 +27,32 @@ class Poisson():
             for i in data:
                 new_lambtha += i / len(data)
             self.lambtha = round(new_lambtha, 2)
+
+    def pmf(self, k):
+        """Calculates the value of the PMF
+        for a given number of successes"""
+        if type(k) is not int:
+            k = int(k)
+
+        try:
+            PMF = self.lambtha ** k * 2.7182818285 ** -self.lambtha / fact(k)
+        except OverflowError:
+            return 0
+
+        return PMF
+
+    def cdf(self, k):
+        """Calculates the value of the CDF
+        for a given number of successes"""
+        if type(k) is not int:
+            k = int(k)
+
+        try:
+            CDF = 0
+            for i in range(k + 1):
+                power1 = self.lambtha ** i
+                power2 = 2.7182818285 ** -self.lambtha
+                CDF += power1 * power2 / fact(i)
+        except OverflowError:
+            return 0
+        return CDF
