807dfef5a5e0a608c6bcc185188fbb9b96545bfb
Cristian G <1154@holbertonschool.com>
2020-07-29 14:09:37 +0000

    Updating task

--- math/0x03-probability/exponential.py
+++ math/0x03-probability/exponential.py
@@ -5,25 +5,26 @@
 class Exponential():
     """represents an exponential distribution"""
     def __init__(self, data=None, lambtha=1.):
-        self.lambtha = float(lambtha)
-        if data is None:
-            if self.lambtha < 0:
-                raise ValueError('lambtha must be a positive value')
-        if data:
+        """constructor"""
+        if data is not None:
             if type(data) is not list:
                 raise TypeError('data must be a list')
-            if len(data) < 2:
+            if len(data) <= 2:
                 raise ValueError('data must contain multiple values')
             result = sum(data) / len(data)
             new_lambtha = 1 / result
             self.lambtha = new_lambtha
+        else:
+            if lambtha <= 0:
+                raise ValueError('lambtha must be a positive value')
+            self.lambtha = float(lambtha)
 
     def pdf(self, x):
         """Calculates the value of the
         Probability Density Function
         for a given time period"""
-        if x > 0:
-            PDF = self.lambtha * 2.7182818285 ** -self.lambtha * x
+        if x >= 0:
+            PDF = self.lambtha * 2.7182818285 ** ((-self.lambtha) * x)
             return PDF
         return 0
 
@@ -31,7 +32,7 @@ class Exponential():
         """Calculates the value of the
         Cumulative Distribution Function
         for a given time period"""
-        if x > 0:
-            CDF = 1 - 2.7182818285 ** -self.lambtha * x
+        if x >= 0:
+            CDF = 1 - 2.7182818285 ** ((-self.lambtha) * x)
             return CDF
         return 0
