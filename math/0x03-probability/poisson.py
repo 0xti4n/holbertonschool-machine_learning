19938c58d35df49f9811a18ccbac657e39ebd941
Cristian G <1154@holbertonschool.com>
2020-07-29 13:56:39 +0000

    Updating task

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -14,17 +14,17 @@ class Poisson():
     a poisson distribution
     """
     def __init__(self, data=None, lambtha=1.):
-        self.lambtha = float(lambtha)
         if data is None:
-            if self.lambtha < 0:
+            if self.lambtha <= 0:
                 raise ValueError('lambtha must be a positive value')
+            self.lambtha = float(lambtha)
         if data:
             if type(data) is not list:
                 raise TypeError('data must be a list')
-            if len(data) < 2:
+            if len(data) <= 2:
                 raise ValueError('data must contain multiple values')
             else:
-                self.lambtha = sum(data) / len(data)
+                self.lambtha = float(sum(data) / len(data))
 
     def pmf(self, k):
         """Calculates the value of the
