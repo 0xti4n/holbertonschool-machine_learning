d494a467821004049bd7d0ba1202fe44f5fab966
Cristian G <1154@holbertonschool.com>
2020-07-29 18:49:29 +0000

    Adding main of task

---
+++ math/0x03-probability/main/1-main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import numpy as np
+Poisson = __import__('poisson').Poisson
+
+np.random.seed(0)
+data = np.random.poisson(5., 100).tolist()
+p1 = Poisson(data)
+print('P(9):', p1.pmf(9))
+
+p2 = Poisson(lambtha=5)
+print('P(9):', p2.pmf(9))
+
+"""
+alexa@ubuntu-xenial:0x03-probability$ 
+P(9): 0.03175849616802446
+P(9): 0.036265577412911795
+"""
