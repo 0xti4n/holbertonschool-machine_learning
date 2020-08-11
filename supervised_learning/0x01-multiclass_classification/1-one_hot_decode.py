ab5d0e29e64b97972a81815c192a542e7ebdf838
Cristian G <1154@holbertonschool.com>
2020-08-11 10:27:15 +0000

    Adding task 1

---
+++ supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""One-Hot decode """
+import numpy as np
+
+
+def one_hot_decode(one_hot):
+    """converts a one-hot matrix
+    into a vector of labels"""
+    if type(one_hot) is not np.ndarray or len(one_hot.shape) != 2:
+        return None
+
+    return np.argmax(one_hot, axis=0)
