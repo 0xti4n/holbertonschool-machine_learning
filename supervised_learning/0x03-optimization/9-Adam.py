ab33e799f8da8bdd31ea2006663b1d22651dbc4e
Cristian G <1154@holbertonschool.com>
2020-08-20 17:42:41 +0000

    Updating task

--- supervised_learning/0x03-optimization/9-Adam.py
+++ supervised_learning/0x03-optimization/9-Adam.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Adam"""
+import numpy as np
 
 
 def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
@@ -9,5 +10,5 @@ def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
     s = beta2 * s + (1 - beta2) * (grad ** 2)
     v_c = v / (1 - beta1 ** t)
     s_c = s / (1 - beta2 ** t)
-    var = var - alpha * v_c / (s_c ** (1 / 2) + epsilon)
+    var = var - alpha * v_c / (np.sqrt(s_c) + epsilon)
     return var, v, s
