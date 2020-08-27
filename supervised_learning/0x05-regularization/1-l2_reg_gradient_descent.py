0350638572c008235949d29745ca153f81912384
Cristian G <1154@holbertonschool.com>
2020-08-27 11:44:08 +0000

    Updating task 1

--- supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -34,7 +34,10 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     error = cache["A" + str(L)] - Y
     for i in range(L, 0, -1):
         w = "W" + str(i)
+        b = "B" + str(i)
         A = cache["A" + str(i - 1)]
         dw = (1 / m) * np.matmul(error, A.T) + lambtha / m * weights[w]
+        db = np.sum(error, axis=1, keepdims=True) / m
         weights[w] = weights[w] - alpha * dw
+        weights[b] = weights[w] - alpha * db
         error = np.matmul(copy_weights["W" + str(i)].T, error) * tanh_f(A)
