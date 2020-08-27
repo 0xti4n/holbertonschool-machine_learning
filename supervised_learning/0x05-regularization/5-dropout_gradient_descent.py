cafc362d67cc65bdb50a7f82abc4723162720920
Cristian G <1154@holbertonschool.com>
2020-08-27 13:33:09 +0000

    Updating task 5

--- supervised_learning/0x05-regularization/5-dropout_gradient_descent.py
+++ supervised_learning/0x05-regularization/5-dropout_gradient_descent.py
@@ -22,17 +22,30 @@ def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
     -> The weights of the network should be updated in place
     """
     m = Y.shape[1]
-
+    dw = {}
+    db = {}
     error = cache["A" + str(L)] - Y
     for i in range(L, 0, -1):
-        w = "W" + str(i)
-        A_p = cache["A" + str(i - 1)]
+        w = 'W' + str(i)
+        b = 'b' + str(i)
+        A_p = cache['A' + str(i - 1)]
         if i == L:
-            dw = (1 / m) * np.matmul(error, A_p.T) / m
+            dw[w] = np.matmul(error, A_p.T) / m
+            db[b] = np.sum(error, axis=1, keepdims=True) / m
+
         else:
+            dw_nxt = "W" + str(i + 1)
+            db_nxt = "b" + str(i + 1)
             error = np.matmul(weights["W" + str(i + 1)].T, error)
             da2 = error * cache["D" + str(i)]
             da2 /= keep_prob
             dz2 = da2 * (1 - (cache["A" + str(i)] * cache["A" + str(i)]))
-            dw = np.matmul(dz2, A_p.T) / m
-            weights[w] -= alpha * dw
+            dw[w] = np.matmul(dz2, A_p.T) / m
+            db[b] = np.sum(dz2, axis=1, keepdims=True) / m
+
+            weights[dw_nxt] -= alpha * dw[dw_nxt]
+            weights[db_nxt] -= alpha * db[db_nxt]
+
+            if i == 1:
+                weights['W1'] -= alpha * dw[w]
+                weights['b1'] -= alpha * db[b]
