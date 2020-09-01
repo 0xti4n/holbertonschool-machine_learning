eedfb700d285e6821d0e08ef9d71b831ca382a68
Cristian G <1154@holbertonschool.com>
2020-09-01 11:50:25 +0000

    Updating task 5

--- supervised_learning/0x06-keras/5-train.py
+++ supervised_learning/0x06-keras/5-train.py
@@ -6,8 +6,7 @@ import tensorflow.keras as K
 def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, verbose=True, shuffle=False):
     """analyze validaiton data"""
-    history = network.fit(
-                          x=data,
+    history = network.fit(x=data,
                           y=labels,
                           batch_size=batch_size,
                           epochs=epochs,
