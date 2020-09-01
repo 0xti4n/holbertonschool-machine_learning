49900f9a838c4975f6f407bfde017b6fd6c20e9d
Cristian G <1154@holbertonschool.com>
2020-09-01 11:48:51 +0000

    Adding task 4

--- supervised_learning/0x06-keras/4-train.py
+++ supervised_learning/0x06-keras/4-train.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """Train"""
+import tensorflow.keras as K
 
 
 def train_model(network, data, labels, batch_size, epochs,
                 verbose=True, shuffle=False):
     """trains a model using mini-batch gradient descent"""
-    history = network.fit(
-                          x=data,
+    history = network.fit(x=data,
                           y=labels,
                           batch_size=batch_size,
                           epochs=epochs,
