1ee9cde38c572b2a4a2446b81237ce4a367b69aa
Cristian G <1154@holbertonschool.com>
2020-09-03 18:39:27 +0000

    Adding main of task

---
+++ math/0x04-convolutions_and_pooling/main/5-main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+import numpy as np
+convolve = __import__('5-convolve').convolve
+
+
+if __name__ == '__main__':
+
+    dataset = np.load('../../supervised_learning/data/animals_1.npz')
+    images = dataset['data']
+    print(images.shape)
+    kernels = np.array([[[[0, 1, 1], [0, 1, 1], [0, 1, 1]], [[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]], [[0, -1, 1], [0, -1, 1], [0, -1, 1]]],
+                       [[[-1, 1, 0], [-1, 1, 0], [-1, 1, 0]], [[5, 0, 0], [5, 0, 0], [5, 0, 0]], [[-1, -1, 0], [-1, -1, 0], [-1, -1, 0]]],
+                       [[[0, 1, -1], [0, 1, -1], [0, 1, -1]], [[-1, 0, -1], [-1, 0, -1], [-1, 0, -1]], [[0, -1, -1], [0, -1, -1], [0, -1, -1]]]])
+
+    images_conv = convolve(images, kernels, padding='valid')
+    print(images_conv.shape)
+
+    plt.imshow(images[0])
+    plt.savefig('5-conA.png')
+    """ plt.show() """
+    plt.imshow(images_conv[0, :, :, 0])
+    plt.savefig('5-conB.png')
+    """ plt.show() """
+    plt.imshow(images_conv[0, :, :, 1])
+    plt.savefig('5-conC.png')
+    """ plt.show() """
+    plt.imshow(images_conv[0, :, :, 2])
+    plt.savefig('5-conD.png')
+    """ plt.show() """
