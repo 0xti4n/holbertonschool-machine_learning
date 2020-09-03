7aff30e0f81aebabc8a772a2e150266de779d6b0
Cristian G <1154@holbertonschool.com>
2020-09-03 18:15:38 +0000

    Adding task 3

--- math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
+++ math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
@@ -30,23 +30,24 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     kh, kw = kernel.shape[0], kernel.shape[1]
     sh, sw = stride
 
-    if padding == 'valid':
-        p_0 = 0
-        p_1 = 0
+    p_0 = 0
+    p_1 = 0
+
     if padding == 'same':
         p_0 = int(((h - 1) * sh + kh - h) / 2) + 1
         p_1 = int(((w - 1) * sw + kw - w) / 2) + 1
+
     if type(padding) == tuple:
         p_0 = padding[0]
         p_1 = padding[1]
 
-    output_h = int(h + 2 * p_0 - kh) / sh + 1
-    output_w = int(w + 2 * p_1 - kw) / sw + 1
+    output_h = int((h + 2 * p_0 - kh) / sh) + 1
+    output_w = int((w + 2 * p_1 - kw) / sw) + 1
     output = np.zeros((m, output_h, output_w))
     img = np.pad(images, ((0, 0), (p_0, p_0), (p_1, p_1)), 'constant')
 
     for x in range(output_h):
         for y in range(output_w):
             slc = img[:, x * sh:sh * x + kh, y * sw: sw * y + kw]
-            output[:, x, y] = np.sum(kernel * slc, axis=1).sum(axis=1)
+            output[:, x, y] = np.sum(slc * kernel, axis=1).sum(axis=1)
     return output
