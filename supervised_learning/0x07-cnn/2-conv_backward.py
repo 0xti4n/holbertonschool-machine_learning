4d3dd2e8182cd3917ada1942aed5a69adf5c0395
Cristian G <1154@holbertonschool.com>
2020-09-09 17:37:51 +0000

    Updating task 2

--- supervised_learning/0x07-cnn/2-conv_backward.py
+++ supervised_learning/0x07-cnn/2-conv_backward.py
@@ -6,40 +6,41 @@ import numpy as np
 def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
     """performs back propagation over a convolutional
     layer of a neural network"""
-    kh, kw, c_prev, c_new = W.shape
-    _, h_new, w_new, c_nw = dZ.shape
-    m, h_prev, w_prev, c_prev = A_prev.shape
-    sh, sw = stride
+    hk = W.shape[0]
+    wk = W.shape[1]
+    nc = W.shape[3]
+    m = A_prev.shape[0]
+    hm = A_prev.shape[1]
+    wm = A_prev.shape[2]
+    st1 = stride[1]
+    st0 = stride[0]
 
     p_0 = 0
     p_1 = 0
 
     if padding == 'same':
-        p_0 = int(((h_prev - 1) * sh + kh - h_prev) / 2) + 1
-        p_1 = int(((w_prev - 1) * sw + kw - w_prev) / 2) + 1
-
-    output_h = int((h_prev + 2 * p_0 - kh) / sh) + 1
-    output_w = int((w_prev + 2 * p_1 - kw) / sw) + 1
+        p_0 = int(((hm - 1) * st0 + hk - hm) / 2) + 1
+        p_1 = int(((wm - 1) * st1 + wk - wm) / 2) + 1
+    
     db = np.sum(dZ, axis=(0, 1, 2), keepdims=True)
-
+    out_h = int((hm + 2 * p_0 - hk) / st0) + 1
+    out_w = int((wm + 2 * p_1 - wk) / st1) + 1
     img = np.pad(A_prev, ((0, 0), (p_0, p_0), (p_1, p_1), (0, 0)), 'constant')
-
+    
+    dX = np.zeros(img.shape)
     dW = np.zeros(W.shape)
-    dx = np.zeros(img.shape)
 
     for i in range(m):
-        for h in range(output_h):
-            for w in range(output_w):
-                for k in range(c_new):
-                    dx[i,
-                        h * sh:h * sh + kh,
-                        w * sw:w * sw + kw, :] += W[:, :, :, k] * dZ[
-                            i, h, w, k]
-                    dW[:, :, :, k] += img[i,
-                                          h * sw:h * sw + kh,
-                                          w * sw:w * sw + kw, :] * dZ[
-                                              i, h, w, k]
-    if padding == 'same':
-        dx = dx[:, p_0:-p_0, p_1:-p_1, :]
+        for h in range(out_h):
+            for w in range(out_w):
+                for c in range(nc):
+                    dX[i, h * st0: h * st0 + hk,
+                       w * st1: w * st1 + wk, :] += dZ[
+                       i, h, w, c] * W[:, :, :, c]
+                    dW[:, :, :, c] += img[i, h * st0: h * st0 + hk,
+                                          w * st1: w * st1 + wk,
+                                          :] * dZ[i, h, w, c]
+    if padding == "same":
+        dX = dX[:, pad0:-pad0, pad1:-pad1, :]
 
-    return dx, dW, db
+    return dX, dW, db
