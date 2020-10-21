abcc852678bc05a7f42d4ff9840eacf182afc15b
cristian gomez <1154@holbertonschool.com>
2020-10-21 14:44:01 -0400

    determinant

--- math/0x05-advanced_linear_algebra/0-determinant.py
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -35,6 +35,9 @@ def determinant(matrix):
     if len(A) == 1:
         return A[0][0]
 
+    if len(A) == 2:
+        return A[0][0] * A[1][1] - A[0][1] * A[1][0]
+
     n = len(A)
     A_copy = A.copy()
 
