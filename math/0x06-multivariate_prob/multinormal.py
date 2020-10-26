2c55a96e235b5bc83ec4e229b9168a9819284a3a
cristian gomez <1154@holbertonschool.com>
2020-10-26 15:42:02 -0400

    Add pdf method of class

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -23,3 +23,28 @@ class MultiNormal():
         cov = data.T.dot(data) / (n - 1)
         self.mean = mean.reshape(d, 1)
         self.cov = cov
+
+    def pdf(self, x):
+        """calculates the PDF at a data point:
+
+        -> x is a numpy.ndarray of shape (d, 1) containing
+        the data point whose PDF should be calculated
+            * d is the number of dimensions of the Multinomial instance
+
+        -> If x is not a numpy.ndarray, raise a TypeError
+        with the message x must be a numpy.ndarray
+
+        -> If x is not of shape (d, 1), raise a
+        ValueError with the message x must have the shape ({d}, 1)
+
+        -> Returns the value of the PDF
+        """
+        N = x.size
+
+        tmp1 = np.linalg.det(self.cov) ** (-1/2)
+        mul1 = -.5 * (x - self.mean).T
+        tmp2 = np.exp(mul1 @ np.linalg.inv(self.cov) @ (x - self.mean))
+
+        res = (2 * np.pi) ** (-N/2) * tmp1 * tmp2
+
+        return np.squeeze(res)
