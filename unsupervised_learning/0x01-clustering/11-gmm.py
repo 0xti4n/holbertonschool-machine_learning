5723ee4add69dc26a6cd76024fba1e2e3616e886
cristian gomez <1154@holbertonschool.com>
2020-11-06 09:12:18 -0500

    Adding task 11

---
+++ unsupervised_learning/0x01-clustering/11-gmm.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""GMM sklearn"""
+import sklearn.mixture as sm
+
+
+def gmm(X, k):
+    """calculates a GMM from a dataset:
+
+    -> X is a numpy.ndarray of shape (n, d)
+        containing the dataset
+
+    -> k is the number of clusters
+
+    -> Returns: pi, m, S, clss, bic
+        * pi is a numpy.ndarray of shape (k,)
+            containing the cluster priors
+        * m is a numpy.ndarray of shape (k, d)
+            containing the centroid means
+        * S is a numpy.ndarray of shape (k, d, d)
+            containing the covariance matrices
+        * clss is a numpy.ndarray of shape (n,)
+            containing the cluster indices for each data point
+        * bic is a numpy.ndarray of shape (kmax - kmin + 1)
+            containing the BIC value for each cluster size tested
+    """
+    if not isinstance(k, int) or k <= 0:
+        return None, None
+
+    gmm = sm.GaussianMixture(n_components=k).fit(X)
+
+    pi = gmm.weights_
+    m = gmm.means_
+    S = gmm.covariances_
+    clss = gmm.predict(X)
+    bic = gmm.bic(X)
+
+    return pi, m, S, clss, bic
