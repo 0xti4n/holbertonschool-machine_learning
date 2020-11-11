d7c2b585dacb98c1b663f734a260ab09c64ee168
cristian gomez <1154@holbertonschool.com>
2020-11-11 15:15:04 -0500

    Updating task 6

--- unsupervised_learning/0x02-hmm/6-baum_welch.py
+++ unsupervised_learning/0x02-hmm/6-baum_welch.py
@@ -127,7 +127,7 @@ def baum_welch(Observations, Transition, Emission, Initial, iterations=1000):
     -> Returns: the converged Transition,
         Emission, or None, None on failure
     """
-    T = Observations.shape[0]
+    """ T = Observations.shape[0]
     M, N = Emission.shape
 
     for n in range(1, iterations):
@@ -161,4 +161,27 @@ def baum_welch(Observations, Transition, Emission, Initial, iterations=1000):
 
         Emission = np.divide(Emission, den.reshape((-1, 1)))
 
-    return Transition, Emission
+    return Transition, Emission """
+    T = Observations.shape[0]
+    M, N = Emission.shape
+
+    for n in range(1, iterations):
+        _, F = forward(Observations, Emission, Transition, Initial)
+        _, B = backward(Observations, Emission, Transition, Initial)
+        xi = np.zeros((M, M, T - 1))
+        for i in range(T - 1):
+            den = np.dot(np.dot(F[:, i].T, Transition) *
+                         Emission[:, Observations[i + 1]].T, B[:, i + 1])
+            for j in range(M):
+                num = F[j, i] * Transition[j] *\
+                    Emission[:, Observations[i + 1]].T * B[:, i + 1].T
+                xi[j, :, i] = num / den
+        gamma = np.sum(xi, axis=1)
+        Transition = np.sum(xi, 2) / np.sum(gamma, axis=1).reshape((-1, 1))
+        gamma = np.hstack((gamma,
+                           np.sum(xi[:, :, T - 2], axis=0).reshape((-1, 1))))
+        denom = np.sum(gamma, axis=1)
+        for i in range(N):
+            Emission[:, i] = np.sum(gamma[:, Observations == i], axis=1)
+        Emission = np.divide(Emission, denom.reshape((-1, 1)))
+    return (Transition, Emission)
