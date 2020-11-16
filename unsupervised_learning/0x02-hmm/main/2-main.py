86b8051db2c3789809c047fc6e667032c9ce90d7
cristian gomez <1154@holbertonschool.com>
2020-11-16 17:05:25 -0500

    Updating main of task 2

--- unsupervised_learning/0x02-hmm/main/2-main.py
+++ unsupervised_learning/0x02-hmm/main/2-main.py
@@ -21,8 +21,13 @@ if __name__ == '__main__':
                   [0, 0.1, 0.5, 0.2, 0.2],
                   [0, 0.1, 0.2, 0.5, .2],
                   [0, 0.1, 0.2, 0.2, 0.5]])
+    f = np.array([[1, 0, 0, 0],
+                  [0, 1, 0, 0],
+                  [0, 0, 0.5, 0.5],
+                  [0, 0.5, 0.5, 0]])
     print(absorbing(a))
     print(absorbing(b))
     print(absorbing(c))
     print(absorbing(d))
     print(absorbing(e))
+    print(absorbing(f))
