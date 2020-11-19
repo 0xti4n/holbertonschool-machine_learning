a6a62c85c3df8ebcffc2a19ca2960e8b9aadeab0
cristian gomez <1154@holbertonschool.com>
2020-11-19 09:19:12 -0500

    Adding task 2

---
+++ unsupervised_learning/0x03-hyperparameter_tuning/2-gp.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+"""Gaussian Process"""
+import numpy as np
+
+
+class GaussianProcess():
+    """represents a noiseless 1D
+    Gaussian process"""
+
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
+        """ Constructor
+
+        -> X_init is a numpy.ndarray of shape (t, 1)
+            representing the inputs already sampled
+            with the black-box function
+
+        -> Y_init is a numpy.ndarray of shape (t, 1)
+            representing the outputs of the black-box
+            function for each input in X_init
+
+        -> t is the number of initial samples
+
+        -> l is the length parameter for the kernel
+
+        -> sigma_f is the standard deviation given to the
+            output of the black-box function
+        """
+        self.X = X_init
+        self.Y = Y_init
+        self.l = l
+        self.sigma_f = sigma_f
+        self.K = self.kernel(self.X, self.X)
+
+    def kernel(self, X1, X2):
+        """calculates the covariance kernel
+            matrix between two matrices
+            Radial Basis Function (RBF)
+
+        -> X1 is a numpy.ndarray of shape (m, 1)
+        -> X2 is a numpy.ndarray of shape (n, 1)
+
+        -> Returns: the covariance kernel matrix as a
+            numpy.ndarray of shape (m, n)
+        """
+        part1 = np.sum(X1**2, 1).reshape(-1, 1)
+        part2 = np.sum(X2**2, 1) - 2 * np.dot(X1, X2.T)
+        sqdist = part1 + part2
+
+        cov = self.sigma_f**2 * np.exp(-0.5 / self.l**2 * sqdist)
+        return cov
+
+    def predict(self, X_s):
+        """predicts the mean and standard
+            deviation of points in a Gaussian process
+
+        -> X_s is a numpy.ndarray of shape (s, 1)
+            containing all of the points whose mean and
+            standard deviation should be calculated
+
+                * s is the number of sample points
+
+        -> Returns: mu, sigma
+            * mu is a numpy.ndarray of shape (s,) containing
+                the mean for each point in X_s, respectively
+            * sigma is a numpy.ndarray of shape (s,) containing
+                the variance for each point in X_s, respectively
+        """
+        s = X_s.shape
+        K = self.K
+        k_s = self.kernel(self.X, X_s)
+        k_ss = self.kernel(X_s, X_s)
+        k_inv = np.linalg.inv(K)
+
+        mu = k_s.T.dot(k_inv).dot(self.Y)
+
+        cov_s = k_ss - k_s.T.dot(k_inv.dot(k_s))
+
+        return np.squeeze(mu), np.diag(cov_s)
+
+    def update(self, X_new, Y_new):
+        """updates a Gaussian Process:
+
+        -> X_new is a numpy.ndarray of shape (1,)
+            that represents the new sample point
+
+        -> Y_new is a numpy.ndarray of shape (1,)
+            that represents the new sample function value
+
+        -> Updates the public instance attributes X, Y, and K
+        """
+        self.X = np.vstack((self.X, X_new))
+        self.Y = np.vstack((self.Y, Y_new))
+        self.K = self.kernel(self.X, self.X)
