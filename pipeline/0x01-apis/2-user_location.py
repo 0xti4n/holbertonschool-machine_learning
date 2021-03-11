aa793a24e01221b75be63fe4d9d7946030bdaca1
cristian gomez <1154@holbertonschool.com>
2021-03-11 13:58:40 -0500

    [add] task 2

--- pipeline/0x01-apis/2-user_location.py
+++ pipeline/0x01-apis/2-user_location.py
@@ -12,14 +12,13 @@ if __name__ == '__main__':
     response = requests.get(url, params=H)
     res_j = response.json()
 
-    if response.status_code == 403:
-        x_limit = response.headers['X-RateLimit-Reset']
-        x = (int(x_limit) - int(time.time())) / 60
-        print('Reset in {} min'.format(x))
-
     if response.status_code == 200:
         print(res_j['location'])
 
-
     if response.status_code == 404:
         print('Not found')
+
+    if response.status_code == 403:
+        x_limit = response.headers['X-RateLimit-Reset']
+        x = (int(x_limit) - int(time.time())) // 60
+        print('Reset in {} min'.format(x))
