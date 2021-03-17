1be7184c0b3ef8cc7bd7ed60cffef1d3b1879eee
cristian gomez <1154@holbertonschool.com>
2021-03-17 14:23:07 -0400

    [add] task

---
+++ pipeline/0x02-databases/106-log_stats.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""Log stats"""
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+
+    count_logs = nginx_collection.count_documents({})
+    path = {'method': 'GET', 'path': '/status'}
+    status = nginx_collection.count_documents(path)
+    method = [
+        "GET",
+        "POST",
+        "PUT",
+        "PATCH",
+        "DELETE"
+    ]
+    print('{} logs'.format(count_logs))
+    print('Methods:')
+    for m in method:
+        n_data = nginx_collection.count_documents({'method': m})
+        print('\tmethod {}: {}'.format(m, n_data))
+
+    print('{} status check'.format(status))
+
+    pipeline = [
+        {'$unwind': '$ip'},
+        {"$sortByCount": '$ip'},
+        {'$limit': 10},
+    ]
+
+    a_ip = nginx_collection.aggregate(pipeline)
+    data_ip = [i for i in a_ip]
+
+    print('IPs:')
+    for ip in data_ip:
+        print('\t{}: {}'.format(ip['_id'], ip['count']))
